50d79a5bf8236eb283db7e6fd0b70649b4036a5d
Quratulain Ashraf <anyashraf874@gmail.com>
2025-11-07 00:49:40 -0700

    Add files via upload

---
+++ js 3 assignment/app.js
@@ -0,0 +1,170 @@
+// 1-
+// var age = prompt("Enter your age:");
+// if (age >= 18) {
+//     alert("You are an adult.");
+// } else {
+//     alert("You are a minor.");
+// }
+
+// 2-
+// var numbers = prompt("Enter any integers :");
+// if (numbers >= +1) {
+//     alert("positive numbers.");
+// } else {
+//     alert("negative numbers.");
+// }
+
+// 3-
+// var numbers = prompt("Enter any integers :");
+// if (numbers % 2 == 0) {
+//     alert("even numbers.");
+// } else  {
+//     alert("odd numbers.");
+// }
+
+// 4-
+// var username = prompt("Enter your username:");
+// if (username == "admin") {
+//     alert("welcome admin!");
+// } else {
+//     alert("You are denied!");
+// }
+
+// 5-
+// let num1=2
+// let num2=6
+// if (num1== num2){
+//     console.log("equal");
+// } else {
+//     console.log("not equal");
+// }
+
+// 6-
+// let num3=3
+// let num4=2
+// if (num3 > num4){
+//     alert("First number is larger ");
+// } else {
+//     alert("Second number is largerl");
+// }
+
+// 7-
+// let anynumber =500
+
+// if (anynumber > 100){
+//     alert("Too big ");
+// } else {
+//     alert("Within range");
+// }
+
+// 8-
+// let Score =10
+
+// if (Score >= 50){
+//    console.log(" Your are Passed! ");
+// } else {
+//    console.log(" Your are failed! ");
+// }
+
+// 9-
+// let temperature =50
+
+// if (temperature >= 30){
+//    console.log(" Its too hot! ");
+// } else {
+//    console.log(" Its  not too hot! ");
+// }
+
+// 10-
+// let num = 20
+
+// if (num == 0){
+//    console.log(" Its zero! " );
+// } else {
+//    console.log(" Its not zero! " );
+// }
+
+// 11-
+// let age = 20
+// if (age >= 60){
+//    console.log("You are a senior citizen.");
+// } else {
+//    console.log("You are not a senior citizen.", );
+// }
+
+// 12- 
+// var username = prompt("Enter your name:");
+// if (username == "Alice") {
+//     alert("Hi, Alice!");
+// } else {
+//     alert("You're not Alice!");
+// }
+
+// 13-
+// let password = 1234567
+// if (password == 12345){
+//    console.log("correct password");
+// } else {
+//    console.log("wrong password" );
+// }
+
+// 14-
+// var currenthour = 17
+// if (currenthour == 0-12){
+//     alert("Good morning!");
+// } else {
+//     alert("Good afternoon!");
+// }
+
+// 15-
+// var numbers = prompt("Enter any integers :");
+// if (numbers %5 == 0) {
+//     console.log("Divisible by 5");
+// } else  {
+//     console.log(" Not divisible by 5");
+// }
+
+// 16-
+//  var user = prompt("want to play a game.");
+// if (user == "yes") {
+//     alert("Let's play the game!");
+// } else {
+//     alert("Maybe next time!");
+// }
+
+// 17-
+// var user = prompt("Enter any color name");
+// if (user  == "blue") {
+//     console.log("Cool choice!");
+// } else  {
+//     console.log("Interesting color!");
+// }
+
+// 18-
+//  var days = prompt("Enter day name.");
+// if (days == "sunday") {
+//     alert("weekend!");
+// } else {
+//     alert("weekday!");
+// }
+
+// 19-
+// var user = prompt("Enter any fruite name");
+// if (user  == "banana") {
+//     console.log("We have that!");
+// } else  {
+//     console.log("Sorry, out of stock.");
+// }
+
+// 20-
+// var gamer = prompt("Type Start");
+// if (gamer == "start") {
+//     alert("Game started!");
+// } else {
+//     aler("Waiting for input...");
+// }
+
+
+                                                        Part- 2
+
+
